f2bb8aa783d1d869eadeb7f968d972a8429177a0
tadaren <fujimoto.teardrop@gmail.com>
2022-07-29 18:32:23 +0900

    ユーザー検索ページを追加

--- app/src/main/java/com/example/learningandroidapp/ui/UserSearch.kt
+++ app/src/main/java/com/example/learningandroidapp/ui/UserSearch.kt
@@ -1,9 +1,6 @@
 package com.example.learningandroidapp.ui
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -13,7 +10,36 @@ import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.learningandroidapp.ui.theme.LearningAndroidAppTheme
+
+
+@Preview
+@Composable
+fun UserSearchScreenPreview() {
+    LearningAndroidAppTheme {
+        UserSearchScreen()
+    }
+}
+
+@Composable
+fun UserSearchScreen() {
+    val userList = List(12) { "ユーザー$it" }
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(text = "ユーザー検索")
+                // TODO 文字サイズとか考える
+            })
+        }
+    ) {
+        Column {
+            SearchBox()
+            UserList(userList = userList)
+        }
+    }
+}
 
 @Composable
 fun SearchBox(modifier: Modifier = Modifier) {
