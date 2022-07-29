f0b8ecd206210778e159a242ad93e9e1671f68f3
tadaren <fujimoto.teardrop@gmail.com>
2022-07-29 18:33:14 +0900

    ユーザ検索ページをMainAcrivityから表示するようにした

--- app/src/main/java/com/example/learningandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/learningandroidapp/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.learningandroidapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.learningandroidapp.ui.UserSearchScreen
 import com.example.learningandroidapp.ui.theme.LearningAndroidAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearningAndroidAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                UserSearchScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearningAndroidAppTheme {
-        Greeting("Android")
     }
 }
