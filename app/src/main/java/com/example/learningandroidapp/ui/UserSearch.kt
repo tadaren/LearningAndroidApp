2ae0956233d4b83dc472813c8e6c526e8ca4c8a3
tadaren <fujimoto.teardrop@gmail.com>
2022-07-29 18:30:51 +0900

    検索結果を表示するリストを追加

--- app/src/main/java/com/example/learningandroidapp/ui/UserSearch.kt
+++ app/src/main/java/com/example/learningandroidapp/ui/UserSearch.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
@@ -45,5 +48,34 @@ fun SearchBox(modifier: Modifier = Modifier) {
         Button(modifier = Modifier.padding(end = 16.dp), onClick = { /*TODO*/ }) {
             Text(text = "検索")
         }
+    }
+}
+
+@Composable
+fun UserList(modifier: Modifier = Modifier, userList: List<String>) {
+    // TODO userList引数の型を再検討
+    LazyColumn {
+        items(items = userList) { user ->
+            UserListItem(user = user)
+        }
+    }
+}
+
+@Composable
+fun UserListItem(modifier: Modifier = Modifier, user: String) {
+    Row(
+        modifier = modifier.padding(top = 8.dp, bottom = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .size(56.dp),
+            color = MaterialTheme.colors.primary,
+            shape = CircleShape,
+        ) {
+            Text(text = "TODO")
+        }
+        Text(modifier = Modifier.padding(start = 16.dp), text = user)
     }
 }
