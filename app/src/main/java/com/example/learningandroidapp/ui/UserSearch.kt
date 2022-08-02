155672185e815f3ed07db8d95e4de1a53d5e5ddd
tadaren <fujimoto.teardrop@gmail.com>
2022-08-02 10:35:36 +0900

    テキストをリソースファイルに移動して読み込むように修正

--- app/src/main/java/com/example/learningandroidapp/ui/UserSearch.kt
+++ app/src/main/java/com/example/learningandroidapp/ui/UserSearch.kt
@@ -14,8 +14,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.learningandroidapp.R
 import com.example.learningandroidapp.ui.theme.LearningAndroidAppTheme
 
 
@@ -35,7 +37,7 @@ fun UserSearchScreen() {
             TopAppBar(
                 title = {
                     Text(
-                        text = "ユーザー検索",
+                        text = stringResource(R.string.appbar_text),
                         style = MaterialTheme.typography.h6
                     )
                 }
@@ -67,14 +69,14 @@ fun SearchBox(modifier: Modifier = Modifier) {
                 .weight(1f),
             value = text,
             onValueChange = { text = it },
-            label = { Text(text = "ユーザー名") },
+            label = { Text(text = stringResource(R.string.searchbox_label)) },
             singleLine = true,
             trailingIcon = {
                 if (!isEmpty) {
                     IconButton(onClick = { text = "" }) {
                         Icon(
                             Icons.Filled.Cancel,
-                            contentDescription = "clear text",
+                            contentDescription = stringResource(R.string.searchbox_clear_button_text),
                             Modifier.size(20.dp)
                         )
                     }
@@ -86,7 +88,7 @@ fun SearchBox(modifier: Modifier = Modifier) {
             })
         )
         Button(modifier = Modifier.padding(end = 16.dp), onClick = { /*TODO*/ }) {
-            Text(text = "検索")
+            Text(text = stringResource(R.string.searchbox_button_text))
         }
     }
 }
@@ -118,7 +120,10 @@ fun UserList(modifier: Modifier = Modifier, userList: List<String>) {
     // TODO userList引数の型を再検討 + Loading表示
     if (userList.isEmpty()) {
         Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = "検索結果なし", style = MaterialTheme.typography.subtitle1)
+            Text(
+                text = stringResource(R.string.userlist_empty_text),
+                style = MaterialTheme.typography.subtitle1
+            )
         }
     } else {
         LazyColumn(modifier = modifier) {
