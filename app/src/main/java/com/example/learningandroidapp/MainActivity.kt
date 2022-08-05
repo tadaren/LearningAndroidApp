8450e3d2893f3ee8c6990067ac9e0b76ab731774
tadaren <fujimoto.teardrop@gmail.com>
2022-08-05 12:00:04 +0900

    @HiltAndroidApp, @AndroidEntryPointを追加

--- app/src/main/java/com/example/learningandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/learningandroidapp/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.learningandroidapp.ui.UserSearchScreen
 import com.example.learningandroidapp.ui.theme.LearningAndroidAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
