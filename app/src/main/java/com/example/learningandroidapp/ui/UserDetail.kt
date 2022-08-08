3a724c88547a3df37aafab8bbb074474c48521fe
tadaren <fujimoto.teardrop@gmail.com>
2022-08-08 18:30:26 +0900

    UserRepoモデルを追加しuiで使用するように変更

--- app/src/main/java/com/example/learningandroidapp/ui/UserDetail.kt
+++ app/src/main/java/com/example/learningandroidapp/ui/UserDetail.kt
@@ -1,10 +1,7 @@
 package com.example.learningandroidapp.ui
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.StarOutline
 import androidx.compose.runtime.Composable
@@ -12,35 +9,37 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.learningandroidapp.models.UserRepo
 import com.example.learningandroidapp.ui.theme.LearningAndroidAppTheme
 
 
 @Preview(showBackground = true)
 @Composable
 fun UserRepositoryCardPreview() {
+    val repo = UserRepo(name = "リポジトリ名", description = "description", language = "Kotlin", star = 0)
     LearningAndroidAppTheme {
-        UserRepositoryCard()
+        UserRepositoryCard(repo)
     }
 }
 
 @Composable
-fun UserRepositoryCard() {
+fun UserRepositoryCard(repo: UserRepo) {
     Card(
         modifier = Modifier
             .fillMaxWidth(), elevation = 4.dp
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = "リポジトリ名")
-            Text(text = "description", style = MaterialTheme.typography.body2)
+            Text(text = repo.name)
+            Text(text = repo.description, style = MaterialTheme.typography.body2)
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.End
             ) {
-                Text(text = "Kotlin", modifier = Modifier.padding(end = 16.dp))
+                Text(text = repo.language, modifier = Modifier.padding(end = 16.dp))
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Icon(Icons.Outlined.StarOutline, contentDescription = "repository star")
-                    Text(text = "star")
+                    Text(text = repo.star.toString())
                 }
             }
         }
