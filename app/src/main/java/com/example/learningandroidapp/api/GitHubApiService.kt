1417ae17bc56cb6b885f0cf4b78d838aaf2f8252
tadaren <fujimoto.teardrop@gmail.com>
2022-08-19 13:59:58 +0900

    ユーザ情報とユーザのリポジトリをAPIから取得する部分を追加

--- app/src/main/java/com/example/learningandroidapp/api/GitHubApiService.kt
+++ app/src/main/java/com/example/learningandroidapp/api/GitHubApiService.kt
@@ -3,6 +3,7 @@ package com.example.learningandroidapp.api
 import com.example.learningandroidapp.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GitHubApiService {
@@ -13,4 +14,17 @@ interface GitHubApiService {
     @GET("search/users")
     suspend fun getUsers(@Query("q") query: String): GetUsersResponse
 
+    @Headers(
+        "Authorization: token ${BuildConfig.GITHUB_TOKEN}",
+        "Accept: application/vnd.github+json"
+    )
+    @GET("users/{username}/repos")
+    suspend fun getUserRepos(@Path("username") userName: String): List<UserRepoApiModel>
+
+    @Headers(
+        "Authorization: token ${BuildConfig.GITHUB_TOKEN}",
+        "Accept: application/vnd.github+json"
+    )
+    @GET("users/{username}")
+    suspend fun getUserDetail(@Path("username") userName: String): UserDetailApiModel
 }
